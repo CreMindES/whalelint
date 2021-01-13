69c2f2154106fcca21dc8d35d165ce9546a9f97f
Tamás G. Barna <cremindes@gmail.com>
2021-01-13 19:11:02 +0000

    Refactor RuleValidationResult into separate file.

--- linter/ruleset/rule.go
+++ linter/ruleset/rule.go
@@ -69,57 +69,6 @@ func (rule Rule) MarshalJSON() ([]byte, error) {
 	})
 }
 
-type RuleValidationResult struct {
-	rule          Rule
-	isViolated    bool
-	LocationRange LocationRange
-}
-
-func (ruleValidationResult *RuleValidationResult) MarshalJSON() ([]byte, error) {
-	return json.Marshal(struct {
-		Rule          Rule          `json:"Rule"`
-		IsViolated    bool          `json:"IsViolated"`
-		LocationRange LocationRange `json:"LocationRange"`
-	} {
-		Rule:          ruleValidationResult.rule,
-		IsViolated:    ruleValidationResult.isViolated,
-		LocationRange: ruleValidationResult.LocationRange,
-	})
-}
-
-func (ruleValidationResult RuleValidationResult) IsViolated() bool {
-	return ruleValidationResult.isViolated
-}
-
-func (ruleValidationResult *RuleValidationResult) SetViolated(params ...bool) {
-	if len(params) == 0 {
-		ruleValidationResult.isViolated = true
-	} else if len(params) == 1 {
-		ruleValidationResult.isViolated = params[0]
-	} else {
-		log.Error("Invalid params to RuleValidationResult::SetViolated")
-	}
-}
-
-func (ruleValidationResult *RuleValidationResult) Location() LocationRange {
-	return ruleValidationResult.LocationRange
-}
-
-func (ruleValidationResult *RuleValidationResult) SetLocation(startLineNUmber, startCharNumber, endLineNumber, endCharNumber int) {
-	ruleValidationResult.LocationRange.start.lineNumber = startLineNUmber
-	ruleValidationResult.LocationRange.start.charNumber = startCharNumber
-	ruleValidationResult.LocationRange.end.lineNumber   = endLineNumber
-	ruleValidationResult.LocationRange.end.charNumber   = endCharNumber
-}
-
-func (ruleValidationResult *RuleValidationResult) SetLocationRangeFrom(locationRange LocationRange) {
-	ruleValidationResult.LocationRange = locationRange
-}
-
-func (ruleValidationResult *RuleValidationResult) SetRule(rule Rule) {
-	ruleValidationResult.rule = rule
-}
-
 func Get() []Rule {
 	return all
 }
