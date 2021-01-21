2476918f83bd438a3f44f96a41e59df5a4f3f8e2
Tamás G. arna <cremindes@gmail.com>
2021-01-21 13:31:29 +0000

    Support LocationRange from buildkit's []parser.Range.

--- linter/ruleset/locationrange.go
+++ linter/ruleset/locationrange.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/moby/buildkit/frontend/dockerfile/instructions"
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
 )
 
 type LocationRange struct {
@@ -53,23 +54,32 @@ func (locationRange LocationRange) MarshalJSON() ([]byte, error) {
 }
 
 func LocationRangeFromCommand(command instructions.Command) LocationRange {
-	location := LocationRange{
-		start: Location{
-			lineNumber: 1,
-			charNumber: 0,
-		},
-		end: Location{
-			lineNumber: 1,
-			charNumber: 0,
-		},
+	return CopyLocationRange(command.Location())
+}
+
+func CopyLocationRange(parserRange []parser.Range) LocationRange {
+	if parserRange == nil {
+		return LocationRange{
+			start: Location{
+				lineNumber: 1,
+				charNumber: 0,
+			},
+			end: Location{
+				lineNumber: 1,
+				charNumber: 0,
+			},
+		}
 	}
 
-	commandLocation := command.Location()
-	if commandLocation != nil {
-		location.start.lineNumber = commandLocation[0].Start.Line
-		location.start.charNumber = commandLocation[0].Start.Character
-		location.end.lineNumber = commandLocation[0].End.Line
-		location.end.charNumber = commandLocation[0].End.Character
+	location := LocationRange{
+		start: Location{
+			lineNumber: parserRange[0].Start.Line,
+			charNumber: parserRange[0].Start.Character,
+		},
+		end: Location{
+			lineNumber: parserRange[len(parserRange)-1].End.Line,
+			charNumber: parserRange[len(parserRange)-1].End.Character,
+		},
 	}
 
 	return location
