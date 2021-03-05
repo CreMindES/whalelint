723df1fbbdbdde0dc42e80e0834e2ca0f1e84a8c
Tamás G.Barna <cremindes@gmail.com>
2021-03-05 19:20:48 +0000

    VSCode extension.

---
+++ plugins/vscode/src/status.ts
@@ -0,0 +1,39 @@
+
+import vscode = require('vscode');
+
+// statusbar item for switching the Go environment
+export let whalelintEnvStatusbarItem: vscode.StatusBarItem;
+export const languageServerIcon = '$(zap)';
+
+export async function initWhalelintStatusBar(port: number) {
+	if (!whalelintEnvStatusbarItem) {
+		whalelintEnvStatusbarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+	}
+
+	// get extension version
+	let extension = vscode.extensions.getExtension("TamasGBarna.whalelint");
+	let extensionVersion = "unknown";
+	if (extension) {
+		extensionVersion = extension.packageJSON.version;
+	}
+
+	// set Go version and command
+	const name = "whlsp";
+
+	whalelintEnvStatusbarItem.text = languageServerIcon + name;
+	whalelintEnvStatusbarItem.tooltip = "WhaleLint v" + extensionVersion + " | Serving on port " + port;
+
+	whalelintEnvStatusbarItem.show();
+}
+
+export async function hideWhalelintStatusBar() {
+	if (whalelintEnvStatusbarItem) {
+		whalelintEnvStatusbarItem.hide();
+		whalelintEnvStatusbarItem.dispose();
+	}	
+}
+
+export async function showWhalelintStatusInStatusBar(port: number) {
+	
+}
+
