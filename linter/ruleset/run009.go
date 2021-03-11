7d0552894bffc209d7653826523c193e6435e223
Tamás G.Barna <cremindes@gmail.com>
2021-03-11 17:18:06 +0000

    bugfix: apt-get update complains about not being passed -y
    GH-53

--- linter/ruleset/run009.go
+++ linter/ruleset/run009.go
@@ -7,7 +7,7 @@ import (
 	Utils "github.com/cremindes/whalelint/utils"
 )
 
-var _ = NewRule("RUN009", "Pass -y|--yes|--assume-yes flag to apt-get in order to be headless.", "",
+var _ = NewRule("RUN009", "Pass assume yes flag to package manager in order to be headless.", "",
 	ValWarning, ValidateRun009)
 
 func ValidateRun009(runCommand *instructions.RunCommand) RuleValidationResult {
@@ -16,18 +16,29 @@ func ValidateRun009(runCommand *instructions.RunCommand) RuleValidationResult {
 		LocationRange: LocationRangeFromCommand(runCommand),
 	}
 
-	packageManagerConfirmOptionMap := map[string][]string{
-		"apt":     {"-y", "--yes", "--assume-yes"},
-		"apt-get": {"-y", "--yes", "--assume-yes"},
-		"dnf":     {"-y", "--assumeyes"},
-		"yum":     {"-y", "--assumeyes"},
-		"zypper":  {"-y", "--no-confirm", "-n", "--non-interactive"},
+	packageManagerConfirmOptionMap := map[string]struct {
+		subcommandSlice []string
+		assumeYesSlice  []string
+	}{
+		"apt":     {[]string{"install", "remove", "purge"}, []string{"-y", "--yes", "--assume-yes"}},
+		"apt-get": {[]string{"install", "remove", "purge"}, []string{"-y", "--yes", "--assume-yes"}},
+		"dnf":     {[]string{"install", "remove", "downgrade"}, []string{"-y", "--assumeyes"}},
+		"yum":     {[]string{"install", "remove", "downgrade"}, []string{"-y", "--assumeyes"}},
+		"zypper": {
+			[]string{"install", "in", "remove", "rm"},
+			[]string{"-y", "--no-confirm", "-n", "--non-interactive"},
+		},
 	}
 
 	bashCommandList := Parser.ParseBashCommandList(runCommand.CmdLine)
 	for _, bashCommand := range bashCommandList {
-		if assumeYesSlice, ok := packageManagerConfirmOptionMap[bashCommand.Bin()]; ok {
-			if !Utils.SliceContains(bashCommand.OptionKeyList(), assumeYesSlice) {
+		if len(bashCommand.SubCommand()) == 0 {
+			continue
+		}
+
+		if pmMap, ok := packageManagerConfirmOptionMap[bashCommand.Bin()]; ok {
+			if Utils.SliceContains(pmMap.subcommandSlice, bashCommand.SubCommand()) &&
+				!Utils.SliceContains(bashCommand.OptionKeyList(), pmMap.assumeYesSlice) {
 				result.SetViolated()
 				result.LocationRange = ParseLocationFromRawParser(bashCommand.Bin(), runCommand.Location())
 			}
